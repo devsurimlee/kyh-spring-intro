c7184154ed8cb4368be3122ee2a929247d207ca5
devsurimlee <devsurimlee@gmail.com>
2022-12-28 23:49:50 +0900

    feat: 프로젝트 환경설정

---
+++ src/main/java/study/kyhspringintro/KyhSpringIntroApplication.java
@@ -0,0 +1,13 @@
+package study.kyhspringintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyhSpringIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyhSpringIntroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/kyhspringintro/KyhSpringIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package study.kyhspringintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyhSpringIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
